f415a65fb7986d57cb2c5b9240680d677391589b
carlos bolorino <carlosbolorino@hotmail.com>
2018-12-17 21:21:29 +0100

    Finishing

---
+++ UrlsAndRoutes/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UrlsAndRoutes.Models;
+
+namespace UrlsAndRoutes.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index() => View("Result",
+            new Result
+            {
+                Controller = nameof(HomeController),
+                Action = nameof(Index)
+            });
+
+        [HttpGet]
+        [ActionName("GetRest")]
+        public string rest(string name) => $"Hola {name}";
+    }
+}
